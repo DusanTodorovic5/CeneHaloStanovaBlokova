bf7279e63e417d0fe7e1025abbc3f48fb3130b36
Dušan Todorović <79877752+DusanTodorovic5@users.noreply.github.com>
2022-03-06 11:35:59 +0100

    fixed script - now searches multiple pages

--- main.py
+++ main.py
@@ -10,46 +10,49 @@ blok = sys.argv[1]
 search = googlesearch.search('prodaja stanova blok ' + blok, num_results = 5)
 
 
+stanovi = []
 
 lSearch = list(search)
 print(lSearch)
 url = lSearch[0]
+page = 1
+while True:
+    urlSearch = url + '?page=' + (str)(page)
+    page = page + 1
+    response = urllib.request.urlopen(urlSearch)
+    webContent = response.read().decode('UTF-8')
+    soup = BeautifulSoup(webContent, "html.parser")
+    mydivs = soup.find_all("div", {"class": "col-md-12 col-sm-12 col-xs-12 col-lg-12"})
+    if (len(mydivs) < 1):
+        break
 
+    for divs in mydivs:#data-value="145.000"><i>
+        stan = []
+        tt = re.search('data-value="(.{0,8})"><',divs.__str__())
+        if (tt):
+            stan.append(tt.group(1))
+        tt = re.search('price-by-surface"><span>(.*)€\/m',divs.__str__())
+        if (tt):
+            stan.append(tt.group(1))
+        #<li class="col-p-1-3"><div class="value-wrapper">107&nbsp;m<sup>2
+        tt = re.search('<div class="value-wrapper">(.{0,10})<sup>2</sup><span class="legend">Kvadratura</span></div>',divs.__str__())
+        if (tt):
+            stan.append(tt.group(1))#
+            #
 
-response = urllib.request.urlopen(url)
-webContent = response.read().decode('UTF-8')
-soup = BeautifulSoup(webContent, "html.parser")
-mydivs = soup.find_all("div", {"class": "col-md-12 col-sm-12 col-xs-12 col-lg-12"})
+        tt = re.search('<div class="value-wrapper">(.{0,10})<span class="legend">Broj soba</span></div>',divs.__str__())
+        if (tt):
+            stan.append(tt.group(1))
 
-#print(mydivs)
-stanovi = []
-for divs in mydivs:#data-value="145.000"><i>
-    stan = []
-    tt = re.search('data-value="(.{0,8})"><',divs.__str__())
-    if (tt):
-        stan.append(tt.group(1))
-    tt = re.search('price-by-surface"><span>(.*)€\/m',divs.__str__())
-    if (tt):
-        stan.append(tt.group(1))
-    #<li class="col-p-1-3"><div class="value-wrapper">107&nbsp;m<sup>2
-    tt = re.search('<div class="value-wrapper">(.{0,10})<sup>2</sup><span class="legend">Kvadratura</span></div>',divs.__str__())
-    if (tt):
-        stan.append(tt.group(1))#
-        #
+        tt = re.search('<div class="value-wrapper">(.{0,10})<span class="legend">Spratnost</span></div>',divs.__str__())
+        if (tt):
+            stan.append(tt.group(1))
 
-    tt = re.search('<div class="value-wrapper">(.{0,10})<span class="legend">Broj soba</span></div>',divs.__str__())
-    if (tt):
-        stan.append(tt.group(1))
-
-    tt = re.search('<div class="value-wrapper">(.{0,10})<span class="legend">Spratnost</span></div>',divs.__str__())
-    if (tt):
-        stan.append(tt.group(1))
-
-    tt = re.search('<h3 class="product-title"><a href="(.*)">(.*)</a></h3>',divs.__str__())
-    if (tt):
-        stan.append(tt.group(2))
-        stan.append(tt.group(1))
-    stanovi.append(stan)
+        tt = re.search('<h3 class="product-title"><a href="(.*)">(.*)</a></h3>',divs.__str__())
+        if (tt):
+            stan.append(tt.group(2))
+            stan.append(tt.group(1))
+        stanovi.append(stan)
 prosek = 0
 for stan in stanovi:
     print("\n##############################\n")
